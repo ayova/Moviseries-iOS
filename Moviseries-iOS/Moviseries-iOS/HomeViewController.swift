a8503a309c7f02c2f3b21be34a21bc1cd413ce15
Agu <agustin.ovari@gmail.com>
2020-08-09 17:48:17 +0200

    Init commit

---
+++ Moviseries-iOS/Moviseries-iOS/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moviseries-iOS
+//
+//  Created by ADMIN on 09/08/2020.
+//  Copyright © 2020 ADMIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
